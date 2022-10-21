cb9748b290d8a8799d6bb189a25916fa0c10f7f3
Emese92 <gregor.emese@gmail.com>
2022-10-21 19:39:20 +0000

    Add function to close burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,13 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -100,6 +103,7 @@ const NavBar = () => {
   return (
     <>
       <Navbar
+        expanded={expanded}
         className={styles.NavBar}
         bg="dark"
         variant="dark"
@@ -112,7 +116,11 @@ const NavBar = () => {
               <img src={logo} alt="logo" height="45px" />
             </Navbar.Brand>
           </NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            ref={ref}
+            onClick={() => setExpanded(!expanded)}
+            aria-controls="basic-navbar-nav"
+          />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-center">
               <NavLink
---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef, useState } from "react";
+
+function useClickOutsideToggle() {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+
+  return {expanded, setExpanded, ref};
+}
+
+export default useClickOutsideToggle;
