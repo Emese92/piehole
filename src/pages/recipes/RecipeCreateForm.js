c88bccb9dc686d508bc5d2c3566579d457ec8da4
Emese92 <gregor.emese@gmail.com>
2022-10-22 14:26:08 +0000

    Add input types and more style to RecipeCreateForm

--- src/pages/recipes/RecipeCreateForm.js
+++ src/pages/recipes/RecipeCreateForm.js
@@ -10,7 +10,9 @@ import {
   // Alert,
 } from "react-bootstrap";
 
+import styles from "../../styles/RecipeCreateEditForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
 
 function RecipeCreateForm() {
   const [errors, setErrors] = useState({});
@@ -21,48 +23,85 @@ function RecipeCreateForm() {
         <Col lg="8">
           <Card className="my-5 rounded-3" style={{ maxWidth: "800px" }}>
             <Card.Body className='p-5 d-flex flex-column align-items-center mx-auto w-100"'>
-              <Card.Title>Add a Recipe</Card.Title>
+              <Card.Title className={styles.Title}>Add a Recipe</Card.Title>
               <Form>
                 <Form.Group>
                   <Form.Label>Recipe Title</Form.Label>
-                  <Form.Control type="text" name="title"></Form.Control>
+                  <Form.Control
+                    className={appStyles.Input}
+                    type="text"
+                    name="title"
+                  ></Form.Control>
                 </Form.Group>
                 <Form.Group>
-                  <Form.Label className="d-flex" htmlFor="image-upload">
-                    Photo
-                  </Form.Label>
-                  <Form.Control></Form.Control>
+                  <Form.Label htmlFor="image-upload">Upload Photo</Form.Label>
+                  <Form.Control
+                    type="file"
+                    accept="image/*"
+                    name="image"
+                  ></Form.Control>
                 </Form.Group>
                 <Row>
                   <Col md="6" className="mb-4">
                     <Form.Group>
-                      <Form.Label>Prep Time</Form.Label>
-                      <Form.Control></Form.Control>
+                      <Form.Label>Prep Time (mins)</Form.Label>
+                      <Form.Control
+                        className={appStyles.Input}
+                        type="number"
+                        name="prep-time"
+                      ></Form.Control>
+                      <small class="form-text text-muted">
+                        Please enter time in minutes
+                      </small>
                     </Form.Group>
                   </Col>
                   <Col md="6" className="mb-4">
                     <Form.Group>
                       <Form.Label>Servings</Form.Label>
-                      <Form.Control></Form.Control>
+                      <Form.Control
+                        className={appStyles.Input}
+                        type="number"
+                        name="serves"
+                        min="0"
+                        max="100"
+                      ></Form.Control>
                     </Form.Group>
                   </Col>
                 </Row>
                 <Form.Group>
+                  <Form.Label>Ingredients</Form.Label>
+                  <Form.Control
+                    className={appStyles.Input}
+                    type="text"
+                    name="ingredients"
+                  ></Form.Control>
+                </Form.Group>
+                <Form.Group>
                   <Form.Label>Directions</Form.Label>
-                  <Form.Control></Form.Control>
+                  <Form.Control
+                    className={appStyles.Input}
+                    type="textarea"
+                    rows={5}
+                    name="steps"
+                  ></Form.Control>
                 </Form.Group>
                 <Row>
-                <Col md="6" className="mb-4">
-
-                <Button className={`${btnStyles.Button} ${btnStyles.Cancel} ${btnStyles.Wide}`} onClick={() => {}}>
-                  cancel
-                </Button>
-                </Col>
-                <Col md="6" className="mb-4">
-                <Button className={`${btnStyles.Button} ${btnStyles.Gray} ${btnStyles.Wide}`} type="submit">
-                  create
-                </Button>
-                </Col>
+                  <Col md="6" className="mb-4">
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Cancel} ${btnStyles.Wide}`}
+                      onClick={() => {}}
+                    >
+                      cancel
+                    </Button>
+                  </Col>
+                  <Col md="6" className="mb-4">
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Gray} ${btnStyles.Wide}`}
+                      type="submit"
+                    >
+                      create
+                    </Button>
+                  </Col>
                 </Row>
               </Form>
             </Card.Body>
